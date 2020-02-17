a8f56261317bdc716a5db31aa883fb452c2dfb89
balakrishna@mantralabsglobal.com <m.balakrishna10@gmail.com>
2020-02-17 18:40:38 +0530

    Added: API to get converted pdf url

--- src/app/components/pdf-generation/pdf-generation.component.ts
+++ src/app/components/pdf-generation/pdf-generation.component.ts
@@ -68,7 +68,6 @@ export class PdfGenerationComponent implements OnInit {
     this.isLoading = false;
     this.ref.detectChanges();
     this.isYouTubeURL(result.url) ? this.createYouTubeContent(result) : this.createPDFContent(result);
-    // this.createContent(result.title, 'application/pdf');
   }
 
 
@@ -129,7 +128,7 @@ export class PdfGenerationComponent implements OnInit {
   }
 
   private createPDFContent(result) {
-    const createdContent = this.createContent(result.title, 'application/pdf');
+    const createdContent = this.createContent(result.title, 'text/html');
 
     createdContent.subscribe(response => {
       console.log('success createContent', response);
@@ -150,7 +149,7 @@ export class PdfGenerationComponent implements OnInit {
           contentType: 'Resource',
           mediaType: 'content',
           code: 'kp.test.res.1',
-          mimeType: mimeType
+          mimeType: 'application/pdf'
         }
       }
     };
@@ -164,7 +163,7 @@ export class PdfGenerationComponent implements OnInit {
     const data = {
       request: {
         content: {
-          fileName: this.parsedContent.title
+          fileName: this.parsedContent.title+'.html'
         }
       }
     };
@@ -181,26 +180,10 @@ export class PdfGenerationComponent implements OnInit {
     );
   }
 
-  hitPresignedUrl(resp){
-    // const url = this.presignedUrl + '/'+resp.contentId
-    // this.http.put(url, data, this.httpOptions)
-    // .subscribe(
-    //   data => {
-    //     console.log('success', data),
-    //     this.sendContentForm(data);
-    //   },
-    //   error => console.log('oops', error)
-    // );
-  }
-
   uploadFile(res) {
     const url = res['result'].pre_signed_url;
-    const blob = this.str2blob(this.parsedContent.content);
-    const file = new File([blob], this.parsedContent.title);
-    const formData = new FormData();
-    formData.append('file', file);
-
-    console.log('file', file);
+    const blob = new Blob([this.parsedContent.content], {type: 'text/html'});
+    const file = new File([blob], `${this.parsedContent.title}.html`, {type: 'text/html'});
 
     const httpOptions = {
       headers: new HttpHeaders({
@@ -209,22 +192,110 @@ export class PdfGenerationComponent implements OnInit {
         'processData': 'false'
       })
     };
-
-    this.http.put(url, formData, httpOptions)
+    console.log('file', file);
+    this.http.put(url, file, httpOptions)
     .subscribe(
       data => {
         console.log('uploadFile success', data);
+        const fileUrl = `${res['result'].pre_signed_url.split('?')[0]}`;
+        this.getConvertedPdfUrl(fileUrl, res['result'].content_id);
+        // this.updateContentWithPDFURL(fileUrl, res['result'].content_id);
       },
       error => console.log('oops uploadFile', error)
     );
   }
 
-  str2blob(str) {
-    return new Blob([str], {type: 'text/html'});
+  getConvertedPdfUrl(fileUrl, contentId){
+    console.log('getConvertedPdfUrl', fileUrl);
+    const url = "http://11.2.6.6/print/v1/print/preview/generate";
+    // let body = new URLSearchParams();
+    // body.set('fileUrl', fileUrl);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'contentType': 'application/json',
+      },
+      )
+    };
+    const data = new FormData();
+    data.append('fileUrl', fileUrl);
+    
+    this.http.post(url, data, httpOptions)
+    .subscribe(
+      (response: any) => {
+        console.log('getConvertedPdfUrl success', response);
+        this.updateContentWithPDFURL(response['result'].pdfUrl, contentId);
+      },
+      error => console.log('oops getConvertedPdfUrl', error)
+    );
+  }
+
+  updateContentWithPDFURL(fileURL, contentId) {
+    // const fileURLTest = "https://sunbirddev.blob.core.windows.net/sunbird-content-dev/print-service/26b4cf10-5162-11ea-9254-abb1d8cfed4e.pdf";
+    console.log('fileURL', fileURL);
+    const data = new FormData();
+    data.append('fileUrl', fileURL);
+    data.append('mimeType', 'application/pdf');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // tslint:disable-next-line:max-line-length
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE',
+        'user-id': 'mahesh',
+        'X-Channel-Id': 'in.ekstep',
+        'enctype': 'multipart/form-data',
+        'processData': 'false',
+        'contentType': 'false',
+        'cache': 'false'
+      })
+    };
+    const url = this.uploadUrl + '/' + contentId;
+    
+    this.http.post(url, data, httpOptions)
+    .subscribe(
+      (response: any) => {
+        console.log('updateContentWithURL success', response);
+        this.successMsgs = {
+          title : 'content created successfully',
+          subTitle: 'Click here to view content',
+          url: `https://devcon.sunbirded.org/play/content/${response['result'].node_id}?contentType=Resource`
+        };
+        console.log('successMsgs', this.successMsgs);
+
+      },
+      error => console.log('oops updateContentWithURL', error)
+    );
   }
 
   openContent(url) {
     chrome.tabs.create({ url: url });
   }
+
+  // createCustomPDF(){
+  //   var base64Img = null;
+  //   var margins = {
+  //     top: 70,
+  //     bottom: 40,
+  //     left: 30,
+  //     width: 550
+  //   };
+  //   var pdf = new jsPDF('p', 'pt', 'a4');
+  //   pdf.setFontSize(18);
+  //   pdf.fromHTML(document.getElementById('html-2-pdfwrapper'), 
+  //     margins.left, // x coord
+  //     margins.top,
+  //     {
+  //       // y coord
+  //       width: margins.width// max width of content on PDF
+  //     },function(dispose) {
+  //       // headerFooterFormatting(pdf)
+  //     }, 
+  //     margins);
+      
+  //   var iframe = document.createElement('iframe');
+  //   iframe.setAttribute('style','position:absolute;right:0; top:0; bottom:0; height:100%; width:650px; padding:20px;');
+  //   document.body.appendChild(iframe);
+    
+  //   iframe.src = pdf.output('datauristring');
+  //   return pdf.output('blob');
+  // }
 
 }
